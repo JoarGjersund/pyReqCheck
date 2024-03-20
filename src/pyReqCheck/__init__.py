88543ea70c440f19364372a9862ee9d07f5196ae
Joar andreas Gjersund <joargjersund@gmail.com>
2024-03-20 21:27:22 +0100

    make executable

--- src/pyReqCheck/__init__.py
+++ src/pyReqCheck/__init__.py
@@ -155,16 +155,25 @@ def compare_lists(list1, list2):
         exit(1)
 
 
-imports = get_all_imports(
-    ".", encoding=None, extra_ignore_dirs=["requirements_check"], follow_links=True
-)
 
-requirements = []
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-for f in files:
-    if f.endswith(".txt"):
-        requirements.extend(parse_requirements(f))
-        print(f)
 
-package_names = [package["name"] for package in requirements]
-compare_lists(imports, package_names)
+def main():
+    imports = get_all_imports(
+        ".", encoding=None, extra_ignore_dirs=["requirements_check"], follow_links=True
+    )
+
+    requirements = []
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        if f.endswith(".txt"):
+            requirements.extend(parse_requirements(f))
+            print(f)
+
+    package_names = [package["name"] for package in requirements]
+    compare_lists(imports, package_names)
+
+if __name__ == "__main__":
+    main()
+
+
+
